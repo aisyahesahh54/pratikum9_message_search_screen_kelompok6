65c51dcd7bc85777463262caa2039b337b5a2e13
Ambarwati Royani <ambarwatiroyanii@gmail.com>
2026-05-04 15:33:18 +0700

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,23 +1,19 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:message_search_screen/models/chat_model.dart';
+import '../models/chat_model.dart';
 
-class ChatService {
-  final String baseUrl =
+class ApiService {
+  final String url =
       "https://api.ppb.widiarrohman.my.id/api/2026/uts/B/kelompok6/chats";
 
-  Future<List<ChatModel>> fetchChats() async {
-    final response = await http.get(Uri.parse(baseUrl));
-
-    print("STATUS: ${response.statusCode}");
-    print("BODY: ${response.body}");
+  Future<List<ChatModel>> getChats() async {
+    final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
+      List list = data['data'];
 
-      List chats = data['data'];
-
-      return chats.map((e) => ChatModel.fromJson(e)).toList();
+      return list.map((e) => ChatModel.fromJson(e)).toList();
     } else {
       throw Exception("Gagal ambil data API");
     }
