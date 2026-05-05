1ec21bd32cef2ff16b898c337fdb738af2a05437
aisyahesahh54 <aisyahesahh54@gmail.com>
2026-05-05 18:13:48 +0700

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,19 +3,19 @@ import 'package:http/http.dart' as http;
 import '../models/profile_model.dart';
 
 class ApiService {
-  static const String baseUrl =
+  static const String url =
       "https://api.ppb.widiarrohman.my.id/api/2026/uts/B/kelompok6/profile";
 
-  static Future<ProfileModel> fetchProfile() async {
-    final response = await http.get(Uri.parse(baseUrl));
+  static Future<ProfileModel> getProfile() async {
+    final response = await http.get(Uri.parse(url));
 
-    print("STATUS: ${response.statusCode}");
+    print("STATUS API: ${response.statusCode}");
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       return ProfileModel.fromJson(data['data']);
     } else {
-      throw Exception("Gagal mengambil data profile");
+      throw Exception("Gagal ambil data profile");
     }
   }
 }
