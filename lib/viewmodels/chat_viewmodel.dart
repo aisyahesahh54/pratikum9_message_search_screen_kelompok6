f41722a6db15886b8ad5a8517570e4848d2da350
aisyahesahh54 <aisyahesahh54@gmail.com>
2026-05-05 16:21:23 +0700

    Update chat_viewmodel.dart

--- lib/viewmodels/chat_viewmodel.dart
+++ lib/viewmodels/chat_viewmodel.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import '../models/chat_model.dart';
 import '../services/api_service.dart';
 
 class ChatViewModel extends ChangeNotifier {
   String message = "";
   bool isLoading = false;
-   bool _isFetched = false; 
+
+  List<ChatModel> chats = [];
 
   Future<void> fetchChats() async {
-
-    print("FETCH DIPANGGIL"); // 
-
     isLoading = true;
     notifyListeners();
 
     try {
+      // ✅ API LAMA (UTS)
       message = await ApiService.fetchMessage();
 
-      print("DATA API: $message"); // 
+      // ✅ API BARU (CHATS)
+      chats = await ApiService.fetchChats();
+
+      print("JUMLAH CHAT: ${chats.length}");
     } catch (e) {
       message = "Error: $e";
       print("ERROR: $e");
