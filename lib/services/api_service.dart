97ac8b5933c9c3ea0a5f35d1baee613c71041719
aisyahesahh54 <aisyahesahh54@gmail.com>
2026-05-05 15:20:36 +0700

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,21 +1,20 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../models/chat_model.dart';
 
 class ApiService {
-  final String url =
-      "https://api.ppb.widiarrohman.my.id/api/2026/uts/B/kelompok6/chats";
+  static const String url =
+      "https://api.ppb.widiarrohman.my.id/api/2026/uts/B/kelompok6/check";
 
-  Future<List<ChatModel>> getChats() async {
+  static Future<String> fetchMessage() async {
+    print("API DIPANGGIL"); // 👈 taruh di sini
+
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      List list = data['data'];
-
-      return list.map((e) => ChatModel.fromJson(e)).toList();
+      print("Response sukses: ${response.body}"); // opsional buat debug
+      return response.body;
     } else {
-      throw Exception("Gagal ambil data API");
+      print("Gagal: ${response.statusCode}"); // opsional debug
+      throw Exception("Gagal ambil data");
     }
   }
 }
